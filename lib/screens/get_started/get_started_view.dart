6505eb5d0a561336e47059ee93e6ba8c8b22dc5b
Adem Emre <ademeg_42@hotmail.com>
2022-05-01 12:04:38 +0300

    Initial commit

---
+++ lib/screens/get_started/get_started_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GetStartedView extends StatelessWidget {
+  const GetStartedView({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
