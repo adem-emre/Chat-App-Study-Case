4ba0ef0e33c0bbee125ac33bc213266b014bbaf9
Adem Emre <ademeg_42@hotmail.com>
2022-05-06 21:55:44 +0300

    edit const

--- lib/constant/app_colors.dart
+++ lib/constant/app_colors.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/painting.dart';
 
-class AppColors{
+class AppColors {
   static const Color primaryColor = Color(0xFF635DE6);
+  static const Color myMessageColor = Color(0xFF635DE6);
+  static const Color friendMessageColor = Color(0xFFE76E99);
   static const Color chatBgColor = Color(0xFFE6E6F6);
+  static const Color secondaryTextColor = Color(0xFFB4B4B4);
+  static const Color getStartedBgColor = Color(0xFFDFECFC);
+  static const Color getStartedBgColor2 = Color(0xFFF7F6FC);
+  static const Color textFieldColor = Color(0xFFF6FCFE);
+  static const Color gradientMessageHeaderColor1 = Color(0xFFEFF6FA);
+  static const Color gradientMessageHeaderColor2 = Color(0xFFD5D5FA);
+  static const Color gradientMessageHeaderColor3 = Color(0xFFD4F0F9);
+  static const Color gradientMessageHeaderColor4 = Color(0xFFE1D7F7); 
+  static const Color archiveTileLeadingColor = Color(0xFFB0AFFD); 
+  static const Color dividerColor = Color(0xFFE8E8E8); 
+  static const Color actionButtonBg = Color(0xFFF1F0FF); 
+
 }
--- lib/constant/app_constants.dart
+++ lib/constant/app_constants.dart
@@ -1,3 +1,12 @@
 class AppConstants{
   static const String appName = "Chatla";
+  static const String getStartedHeader = "Getting closer to your circle with us";
+  static const String getStartedContent = "Dont't forget to catch up with your friend for making the best connections";
+  static const String getStartedButton = "Get Started";
+  static const String textFieldHint = "What are you looking for?"; 
+  static const String archivedMessages = "Archived Messages"; 
+  static const String callsTab = "Calls";
+  static const String messagesTab = "Messages";
+  static const String directoryTab = "Directory";
+
 }
