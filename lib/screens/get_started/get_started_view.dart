703395a9ebf586b8d434cc98ca247f913a5d0e10
Adem Emre <ademeg_42@hotmail.com>
2022-05-06 21:43:56 +0300

    edit get_started_view

--- lib/screens/get_started/get_started_view.dart
+++ lib/screens/get_started/get_started_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:study_case/screens/home/home_view.dart';
 import '../../constant/app_colors.dart';
 import '../../constant/app_constants.dart';
 import '../../core/extensions/context_ext.dart';
@@ -8,20 +9,19 @@ import '../../core/widgets/custom_button.dart';
 
 class GetStartedView extends StatelessWidget {
   const GetStartedView({Key? key}) : super(key: key);
+  final EdgeInsetsGeometry textPadding =
+      const EdgeInsets.fromLTRB(15, 15, 15, 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: Container(
         decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            AppColors.getStartedBgColor2,
-            AppColors.getStartedBgColor,
-            AppColors.getStartedBgColor2,
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter
-          )
-        ),
+            gradient: LinearGradient(colors: [
+          AppColors.getStartedBgColor2,
+          AppColors.getStartedBgColor,
+          AppColors.getStartedBgColor2,
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
         padding: const EdgeInsets.all(10),
         width: context.width,
         child: Column(
@@ -32,39 +32,55 @@ class GetStartedView extends StatelessWidget {
               style: GoogleFonts.lexendDeca(
                   color: AppColors.primaryColor, fontSize: 30),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Icon(
               Icons.message,
               color: AppColors.primaryColor,
               size: context.setScaledWidth(0.4),
             ),
             SizedBox(height: context.setScaledHeight(0.05)),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                AppConstants.getStartedHeader,
-                textAlign: TextAlign.center,
-                style: GoogleFonts.lexendDeca(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+            _getStartedHeader(),
+            _getStartedContent(),
+            SizedBox(
+              height: context.setScaledHeight(0.05),
             ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                AppConstants.getStartedContent,
-                textAlign: TextAlign.center,
-                style: GoogleFonts.lexendDeca(
-                  color: AppColors.secondaryTextColor,
-                ),
-              ),
+            CustomButton(
+              label: AppConstants.getStartedButton,
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => const HomeView()));
+              },
             ),
-            CustomButton(label: AppConstants.getStartedButton,onPressed: (){},),
           ],
         ),
       ),
     );
   }
-}
 
+  Padding _getStartedHeader() {
+    return Padding(
+      padding: textPadding,
+      child: Text(
+        AppConstants.getStartedHeader,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.lexendDeca(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Padding _getStartedContent() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Text(
+        AppConstants.getStartedContent,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.lexendDeca(
+          color: AppColors.secondaryTextColor,
+        ),
+      ),
+    );
+  }
+}
