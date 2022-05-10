df0e335da0e426db186d18bfbc25a8232c96b21c
Adem Emre <ademeg_42@hotmail.com>
2022-05-10 17:19:57 +0300

    edit chat_view

--- lib/screens/chat/chat_view.dart
+++ lib/screens/chat/chat_view.dart
@@ -26,68 +26,76 @@ class ChatView extends StatelessWidget {
           children: [
             ChatHeader(
                 selectedUser: context.read<ChatCubit>().state.selectedUser!),
-            Expanded(child: BlocBuilder<ChatCubit, ChatState>(
-              builder: (context, state) {
-                return ListView.builder(
-                  controller: _scrollController,
-                    itemCount: state.chatMessages.length,
-                    itemBuilder: (context, index) {
-                      return ChatMessageBox(
-                        message: state.chatMessages[index],
-                      );
-                    });
-              },
-            )),
-            Container(
-              color: Colors.white,
-              height: context.setScaledHeight(0.15),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: footerItemPadding,
-                    child: const ChatActionButton(
-                        icon: Icon(
-                      Icons.image,
-                      color: AppColors.primaryColor,
-                    )),
-                  ),
-                  Expanded(
-                    child: CustomTextField(
-                        controller:
-                            context.read<ChatCubit>().textEditingController,
-                        hintText: AppConstants.yourMessage,
-                        suffix: IconButton(
-                          onPressed: () {
-                            context.read<ChatCubit>().sendMessage(Message(
-                                message: context
-                                    .read<ChatCubit>()
-                                    .textEditingController
-                                    .text
-                                    .trim(),
-                                isFromMe: true));
-                             _scrollController.jumpTo(_scrollController.position.maxScrollExtent+extraScrollValue);
-
-                          },
-                          icon: const Icon(
-                            Icons.send,
-                            color: AppColors.primaryColor,
-                          ),
-                        )),
-                  ),
-                  Padding(
-                    padding: footerItemPadding,
-                    child: const ChatActionButton(
-                        icon: Icon(
-                      Icons.mic,
-                      color: Colors.teal,
-                    )),
-                  ),
-                ],
-              ),
-            )
+            _messagesSection(),
+            _senMessageSection(context)
           ],
         ),
       ),
     );
+  }
+
+  Expanded _messagesSection() {
+    return Expanded(child: BlocBuilder<ChatCubit, ChatState>(
+            builder: (context, state) {
+              return ListView.builder(
+                controller: _scrollController,
+                  itemCount: state.chatMessages.length,
+                  itemBuilder: (context, index) {
+                    return ChatMessageBox(
+                      message: state.chatMessages[index],
+                    );
+                  });
+            },
+          ));
+  }
+
+  Container _senMessageSection(BuildContext context) {
+    return Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              children: [
+                Padding(
+                  padding: footerItemPadding,
+                  child: const ChatActionButton(
+                      icon: Icon(
+                    Icons.image,
+                    color: AppColors.primaryColor,
+                  )),
+                ),
+                Expanded(
+                  child: CustomTextField(
+                      controller:
+                          context.read<ChatCubit>().textEditingController,
+                      hintText: AppConstants.yourMessage,
+                      suffix: IconButton(
+                        onPressed: () {
+                          context.read<ChatCubit>().sendMessage(Message(
+                              message: context
+                                  .read<ChatCubit>()
+                                  .textEditingController
+                                  .text
+                                  .trim(),
+                              isFromMe: true));
+                           _scrollController.jumpTo(_scrollController.position.maxScrollExtent+extraScrollValue);
+
+                        },
+                        icon: const Icon(
+                          Icons.send,
+                          color: AppColors.primaryColor,
+                        ),
+                      )),
+                ),
+                Padding(
+                  padding: footerItemPadding,
+                  child: const ChatActionButton(
+                      icon: Icon(
+                    Icons.mic,
+                    color: Colors.teal,
+                  )),
+                ),
+              ],
+            ),
+          );
   }
 }
